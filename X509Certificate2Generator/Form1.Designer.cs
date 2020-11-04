2051a7c80030be46ff352b56f3334649fbde8f6f
Mohammad Fneish <mohammadfneish7@gmail.com>
2020-11-04 20:50:00 +0200

    First Push

---
+++ X509Certificate2Generator/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace X509Certificate2Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCAName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCACertPath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCACertPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(128, 114);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(245, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Certificate Name:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(361, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Certificate Password:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(128, 140);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(245, 20);
+            this.txtPass.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(128, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(164, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Generate new CA Certificate";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "CA Name:";
+            // 
+            // txtCAName
+            // 
+            this.txtCAName.Enabled = false;
+            this.txtCAName.Location = new System.Drawing.Point(128, 35);
+            this.txtCAName.Name = "txtCAName";
+            this.txtCAName.Size = new System.Drawing.Size(245, 20);
+            this.txtCAName.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "CA Cert Path:";
+            // 
+            // txtCACertPath
+            // 
+            this.txtCACertPath.Location = new System.Drawing.Point(128, 61);
+            this.txtCACertPath.Name = "txtCACertPath";
+            this.txtCACertPath.ReadOnly = true;
+            this.txtCACertPath.Size = new System.Drawing.Size(206, 20);
+            this.txtCACertPath.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 20);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "CA Cert Password:";
+            // 
+            // txtCACertPass
+            // 
+            this.txtCACertPass.Location = new System.Drawing.Point(128, 88);
+            this.txtCACertPass.Name = "txtCACertPass";
+            this.txtCACertPass.PasswordChar = '*';
+            this.txtCACertPass.Size = new System.Drawing.Size(245, 20);
+            this.txtCACertPass.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 245);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCACertPass);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCACertPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCAName);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Self-Signed X509Certificate Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCAName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCACertPath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCACertPass;
+    }
+}
+
---
+++ X509Certificate2Generator/Form1.cs
@@ -0,0 +1,294 @@
+﻿using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Prng;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X509Certificate2Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtCACertPass.Text) || (checkBox1.Checked && string.IsNullOrWhiteSpace(txtCAName.Text)) || (!checkBox1.Checked && string.IsNullOrWhiteSpace(txtCACertPath.Text)) || string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtName.Text))
+                {
+                    MessageBox.Show("Make sure you fill required information to continue.");
+                    return;
+                }
+
+                AsymmetricKeyParameter caprivateKey = null;
+                X509Certificate2 caCert = checkBox1.Checked ? GenerateCACertificate(txtCAName.Text.Trim(), ref caprivateKey) : ImportCertificate(txtCACertPath.Text, txtCACertPass.Text);
+                if (caprivateKey == null)
+                {
+                    caprivateKey = TransformRSAPrivateKey(caCert.PrivateKey);
+                }
+
+                X509Certificate2 cert = GenerateSelfSignedCertificate(txtName.Text.Trim(), caCert.Subject, caprivateKey);
+
+
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        SaveCert(cert, fbd.SelectedPath, cert.Subject, txtPass.Text);
+                        if (checkBox1.Checked)
+                        {
+                            SaveCert(caCert, fbd.SelectedPath, caCert.Subject, txtCACertPass.Text);
+                        }
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        public static void SaveCert(X509Certificate2 cert, string path, string name, string pass)
+        {
+            if (name.StartsWith("CN=", StringComparison.InvariantCultureIgnoreCase))
+                name = name.Remove(0, 3);
+            byte[] certData = cert.Export(X509ContentType.Pfx, pass);
+            File.WriteAllBytes(Path.Combine(path, $"{name}.pfx"), certData);
+            MessageBox.Show($"'{name}' Certificate Created!");
+        }
+
+        public static AsymmetricKeyParameter TransformRSAPrivateKey(AsymmetricAlgorithm privateKey)
+        {
+            RSACryptoServiceProvider prov = privateKey as RSACryptoServiceProvider;
+            RSAParameters parameters = prov.ExportParameters(true);
+
+            return new RsaPrivateCrtKeyParameters(
+                new BigInteger(1, parameters.Modulus),
+                new BigInteger(1, parameters.Exponent),
+                new BigInteger(1, parameters.D),
+                new BigInteger(1, parameters.P),
+                new BigInteger(1, parameters.Q),
+                new BigInteger(1, parameters.DP),
+                new BigInteger(1, parameters.DQ),
+                new BigInteger(1, parameters.InverseQ));
+        }
+
+        internal static X509Certificate2 ImportCertificate(string certPath, string pass)
+        {
+            X509Certificate2 x509Certificate2 = null;
+
+            try
+            {
+                // Create a collection object and populate it using the PFX file
+                X509Certificate2Collection collection = new X509Certificate2Collection();
+                collection.Import(certPath, pass, X509KeyStorageFlags.PersistKeySet);
+                x509Certificate2 = collection.Count > 0 ? collection[0] : null;
+                if (x509Certificate2 != null)
+                    return x509Certificate2;
+                else
+                    throw new Exception($"Failed to load Https certificate from the following path: {certPath}");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public static X509Certificate2 GenerateSelfSignedCertificate(string subjectName, string issuerName, AsymmetricKeyParameter issuerPrivKey)
+        {
+            const int keyStrength = 2048;
+
+            // Generating Random Numbers
+            CryptoApiRandomGenerator randomGenerator = new CryptoApiRandomGenerator();
+            SecureRandom random = new SecureRandom(randomGenerator);
+
+            // The Certificate Generator
+            X509V3CertificateGenerator certificateGenerator = new X509V3CertificateGenerator();
+
+            // Serial Number
+            BigInteger serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(Int64.MaxValue), random);
+            certificateGenerator.SetSerialNumber(serialNumber);
+
+            // Signature Algorithm
+            const string signatureAlgorithm = "SHA256WithRSA";
+            certificateGenerator.SetSignatureAlgorithm(signatureAlgorithm);
+
+            // Issuer and Subject Name
+            X509Name subjectDN = new X509Name($"CN={subjectName}");
+            X509Name issuerDN = new X509Name($"CN={issuerName}");
+            certificateGenerator.SetIssuerDN(issuerDN);
+            certificateGenerator.SetSubjectDN(subjectDN);
+
+            // Valid For
+            DateTime notBefore = DateTime.UtcNow.Date;
+            DateTime notAfter = notBefore.AddYears(2);
+
+            certificateGenerator.SetNotBefore(notBefore);
+            certificateGenerator.SetNotAfter(notAfter);
+
+            // Subject Public Key
+            AsymmetricCipherKeyPair subjectKeyPair;
+            var keyGenerationParameters = new KeyGenerationParameters(random, keyStrength);
+            var keyPairGenerator = new RsaKeyPairGenerator();
+            keyPairGenerator.Init(keyGenerationParameters);
+            subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+
+            certificateGenerator.SetPublicKey(subjectKeyPair.Public);
+
+            // Generating the Certificate
+            AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
+
+            // Selfsign certificate
+            Org.BouncyCastle.X509.X509Certificate certificate = certificateGenerator.Generate(issuerPrivKey, random);
+
+            // Corresponding private key
+            PrivateKeyInfo info = PrivateKeyInfoFactory.CreatePrivateKeyInfo(subjectKeyPair.Private);
+
+            // Merge into X509Certificate2
+            X509Certificate2 x509 = new X509Certificate2(certificate.GetEncoded());
+
+            Asn1Sequence seq = (Asn1Sequence)info.ParsePrivateKey();
+            if (seq.Count != 9)
+            {
+                //throw new PemException("malformed sequence in RSA private key");
+            }
+
+            RsaPrivateKeyStructure rsa = new RsaPrivateKeyStructure(seq);
+            RsaPrivateCrtKeyParameters rsaparams = new RsaPrivateCrtKeyParameters(
+                rsa.Modulus, rsa.PublicExponent, rsa.PrivateExponent, rsa.Prime1, rsa.Prime2, rsa.Exponent1, rsa.Exponent2, rsa.Coefficient);
+
+            x509.PrivateKey = DotNetUtilities.ToRSA(rsaparams);
+            return x509;
+
+        }
+
+        public static X509Certificate2 GenerateCACertificate(string subjectName, ref AsymmetricKeyParameter CaPrivateKey)
+        {
+            const int keyStrength = 2048;
+
+            // Generating Random Numbers
+            CryptoApiRandomGenerator randomGenerator = new CryptoApiRandomGenerator();
+            SecureRandom random = new SecureRandom(randomGenerator);
+
+            // The Certificate Generator
+            X509V3CertificateGenerator certificateGenerator = new X509V3CertificateGenerator();
+
+            // Serial Number
+            BigInteger serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(Int64.MaxValue), random);
+            certificateGenerator.SetSerialNumber(serialNumber);
+
+            // Signature Algorithm
+            const string signatureAlgorithm = "SHA256WithRSA";
+            certificateGenerator.SetSignatureAlgorithm(signatureAlgorithm);
+
+            // Issuer and Subject Name
+            X509Name subjectDN = new X509Name($"CN={subjectName}");
+            X509Name issuerDN = subjectDN;
+            certificateGenerator.SetIssuerDN(issuerDN);
+            certificateGenerator.SetSubjectDN(subjectDN);
+
+            // Valid For
+            DateTime notBefore = DateTime.UtcNow.Date;
+            DateTime notAfter = notBefore.AddYears(2);
+
+            certificateGenerator.SetNotBefore(notBefore);
+            certificateGenerator.SetNotAfter(notAfter);
+
+            // Subject Public Key
+            AsymmetricCipherKeyPair subjectKeyPair;
+            KeyGenerationParameters keyGenerationParameters = new KeyGenerationParameters(random, keyStrength);
+            RsaKeyPairGenerator keyPairGenerator = new RsaKeyPairGenerator();
+            keyPairGenerator.Init(keyGenerationParameters);
+            subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+
+            certificateGenerator.SetPublicKey(subjectKeyPair.Public);
+
+            // Generating the Certificate
+            AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
+
+            // Selfsign certificate
+            Org.BouncyCastle.X509.X509Certificate certificate = certificateGenerator.Generate(issuerKeyPair.Private, random);
+            X509Certificate2 x509 = new System.Security.Cryptography.X509Certificates.X509Certificate2(certificate.GetEncoded());
+
+            CaPrivateKey = issuerKeyPair.Private;
+
+            return x509;
+            //return issuerKeyPair.Private;
+        }
+
+        public static bool addCertToStore(System.Security.Cryptography.X509Certificates.X509Certificate2 cert, System.Security.Cryptography.X509Certificates.StoreName st, System.Security.Cryptography.X509Certificates.StoreLocation sl)
+        {
+            bool bRet = false;
+
+            X509Store store = new X509Store(st, sl);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(cert);
+
+            store.Close();
+            return bRet;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtCAName.Enabled = true;
+                txtCACertPath.Enabled = false;
+                button2.Enabled = false;
+            }
+            else
+            {
+                txtCAName.Enabled = false;
+                txtCACertPath.Enabled = true;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Pfx File";
+            theDialog.Filter = "PFX files|*.pfx";
+            theDialog.Multiselect = false;
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    txtCACertPath.Text = theDialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
